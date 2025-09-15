44b033cbbff7d20208a406f52b3d6a9c45c54f66
Kendrick Shen <kshenshen@gmail.com>
2025-09-15 12:50:30 -0700

    init

---
+++ python_websocket_tools/create_tool.py
@@ -0,0 +1,30 @@
+from phonic import Phonic, ToolParameter
+
+client = Phonic()
+
+client.tools.create(
+    name="find_flights_sync",
+    description=f"Find one-way flights on a given date from an origin to a destination airport.",
+    type="custom_websocket",
+    execution_mode="sync",
+    parameters=[
+        ToolParameter(
+            type="string",
+            name="date",
+            description="The date of the flight in YYYY-MM-DD format",
+            is_required=True,
+        ),
+        ToolParameter(
+            type="string",
+            name="from_airport",
+            description="The origin airport code",
+            is_required=True,
+        ),
+        ToolParameter(
+            type="string",
+            name="to_airport",
+            description="The destination airport code",
+            is_required=True,
+        ),
+    ],
+)
