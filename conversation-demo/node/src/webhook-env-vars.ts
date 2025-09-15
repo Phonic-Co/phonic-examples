bd56c8f43b7723d64cb98fa45540e85572581a87
Moin Nadeem <moinnadeem@moinnadeem.com>
2025-09-15 15:53:16 -0700

    Remove conversation-demo example

--- conversation-demo/node/src/webhook-env-vars.ts
+++
@@ -1,9 +0,0 @@
-import { config } from "dotenv";
-
-config({ path: ".env.local" });
-
-const phonicWebhookSecret = process.env.PHONIC_WEBHOOK_SECRET;
-const phonicConfigWebhookAuthorization =
-  process.env.PHONIC_CONFIG_WEBHOOK_AUTHORIZATION;
-
-export { phonicConfigWebhookAuthorization, phonicWebhookSecret };
