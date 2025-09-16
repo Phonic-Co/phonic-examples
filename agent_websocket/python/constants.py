ef5ed2afa87ccce08e0fa569019a763b252c79e8
Kendrick Shen <kshenshen@gmail.com>
2025-09-15 18:10:03 -0700

    update

--- agent_websocket/python/constants.py
+++ agent_websocket/python/constants.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
-CUSTOMER_NAME = "Kendrick"
+CUSTOMER_NAME = "<your name>"
 TODAY_DATE = datetime.now().strftime("%Y-%m-%d")
-NGROK_URL = "51feec1bd397.ngrok-free.app"
+NGROK_URL = "<your ngrok url>"
+
+TWILIO_PHONE_NUMBER = "<your twilio phone number>"
+CUSTOMER_PHONE_NUMBER = "<your phone number>"
