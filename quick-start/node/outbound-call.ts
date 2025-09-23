c043b02a7cb05f2c430b9295e0ad148bb40fc6f8
Misha Moroshko <michael.moroshko@gmail.com>
2025-09-23 20:55:44 +1000

    Update phone number to placeholder

--- quick-start/node/outbound-call.ts
+++ quick-start/node/outbound-call.ts
@@ -10,7 +10,7 @@ const client = new PhonicClient({
 
 async function outboundCall() {
   await client.conversations.outboundCall({
-    to_phone_number: "+61479056111", // e.g. +19189391262
+    to_phone_number: "YOUR_PHONE_NUMBER", // e.g. +19189391262
     config: {
       agent: "my-first-agent",
       welcome_message: "Hello, how can I help you?",
