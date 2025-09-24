6772e6334138afe108ed9a71edbf8d8ba8839a62
Kendrick Shen <32920124+kshen6@users.noreply.github.com>
2025-09-24 10:38:01 -0700

    init (#24)
    Co-authored-by: Kendrick Shen <kshenshen@gmail.com>

--- quick-start/python/outbound_call.py
+++ quick-start/python/outbound_call.py
@@ -9,10 +9,16 @@ client = Phonic(
     api_key=os.getenv("PHONIC_API_KEY"),
 )
 
-client.conversations.outbound_call(
-    to_phone_number=os.getenv("CUSTOMER_PHONE_NUMBER", "") # e.g. +15551234567
-    config={
-        "agent": "my-first-agent",
-        "welcome_message": "Hello, how can I help you?",
-    },
-)
+
+def outbound_call():
+    client.conversations.outbound_call(
+        to_phone_number=os.getenv("CUSTOMER_PHONE_NUMBER"),
+        config={
+            "agent": "my-first-agent",
+            "welcome_message": "Hello, how can I help you?",
+        },
+    )
+
+
+if __name__ == "__main__":
+    outbound_call()
